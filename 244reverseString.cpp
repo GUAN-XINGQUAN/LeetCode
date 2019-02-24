b7ede45be7f27fd5b6a249ff6b3b5c8893e868ec
XINGQUAN GUAN <619460794@qq.com>
2019-02-24 11:57:32 -0800

    Update 244reverseString.cpp

--- 244reverseString.cpp
+++ 244reverseString.cpp
@@ -8,10 +8,13 @@ public:
 	void reverseString(vector<char>& s) 
 	{
 		int left = 0, right = s.size() - 1;
-		while (left < right) {
-			char t = s[left];
-			s[left++] = s[right];
-			s[right--] = t;
+		while (left < right)
+		{
+			char temp = s[left];
+			s[left] = s[right];
+			s[right] = temp;
+			++left;
+			--right;
 		}
 	}
 };
