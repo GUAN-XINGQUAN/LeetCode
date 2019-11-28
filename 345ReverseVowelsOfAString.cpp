becdc79ad70b6fa09c32060f7a5b80010ac25d73
XINGQUAN GUAN <619460794@qq.com>
2019-11-28 13:39:20 -0800

    Update 345ReverseVowelsOfAString.cpp

--- 345ReverseVowelsOfAString.cpp
+++ 345ReverseVowelsOfAString.cpp
@@ -1,44 +1,71 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
+#include<set>
+
 using namespace std;
 
+// Method 1: use STL set
+//class Solution {
+//public:
+//	string reverseVowels(string s) {
+//		set<char> st = { 'a', 'e', 'i', 'o', 'u', 'A' ,'E', 'I', 'O', 'U'};
+//		int left = 0, right = s.size() - 1;
+//		while (left < right)
+//		{
+//			if (st.count(s[left]) == 0)
+//				left++;
+//			else if (st.count(s[right]) == 0)
+//				right--;
+//			else
+//				swap(s[left++], s[right--]);
+//		}
+//		return s;
+//	}
+//};
+
+// Method 2: use user-defined isVowel function
 class Solution
 {
 public:
 	string reverseVowels(string s)
 	{
-		int first = 0;
-		int last = s.size();
-		while (first < last)
+		int left = 0, right = s.size() - 1;
+		while (left < right)
 		{
-			if (isVowels(s[first]) && isVowels(s[last]))
+			if (isVowels(s[left]) && isVowels(s[right]))
 			{
-				char temp = s[first];
-				s[first] = s[last];
-				s[last] = temp;
-				first++;
-				last--;
+				char temp = s[left];
+				s[left] = s[right];
+				s[right] = temp;
+				left++, right--;
 			}
-			else if (isVowels(s[first]))
-				last--;
+			else if (isVowels(s[left]) && !isVowels(s[right]))
+				right--;
+			else if (!isVowels(s[left]) && isVowels(s[right]))
+				left++;
 			else
-				first++;
+			{
+				left++, right--;
+			}
 		}
 		return s;
 	}
-	bool isVowels(char c)
+	bool isVowels(char ch)
 	{
-		if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
-			c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+		if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+			ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
 			return true;
 		else
 			return false;
 	}
+
 };
 
 int main()
 {
-	string str = "hello";
 	Solution sol;
-	cout << sol.reverseVowels(str) << endl;
+	string s = "hello";
+	string result = sol.reverseVowels(s);
+
+	cout << "The result is: " << result << endl;
 }
