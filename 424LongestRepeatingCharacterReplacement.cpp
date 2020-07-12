92362a9b137d6514b22521f80ace728854a0bd17
XINGQUAN GUAN <619460794@qq.com>
2020-07-12 15:30:08 -0700

    Create 424LongestRepeatingCharacterReplacement.cpp

---
+++ 424LongestRepeatingCharacterReplacement.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+
+// #include "nestedVectorIntVisualization.h"
+// #include "nestedVectorCharVisualization.h"
+// #include "treeVisualizationinPreorder.h"
+// #include "listNodeVisualization.h"
+
+using namespace std;
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> counts(26, 0);
+        int res = 0, maxCount = 0, start = 0;
+        for (int i = 0; i < s.size(); i++)
+        {
+            ++counts[s[i] - 'A'];
+            maxCount = max(maxCount, counts[s[i] - 'A']);
+            while (i - start + 1 - maxCount > k)
+            {
+                --counts[s[i] - 'A'];
+                ++start;
+            }
+            res = max(res, i - start + 1);
+        }
+        return res;
+    }
+};
+
+int main()
+{
+
+}
