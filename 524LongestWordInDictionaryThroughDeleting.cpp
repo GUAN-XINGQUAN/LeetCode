60904e995ecf8129421c9ab48de4c15c9241dc53
XINGQUAN GUAN <619460794@qq.com>
2020-07-19 18:12:04 -0700

    Update 524LongestWordInDictionaryThroughDeleting.cpp

--- 524LongestWordInDictionaryThroughDeleting.cpp
+++ 524LongestWordInDictionaryThroughDeleting.cpp
@@ -1,75 +1,113 @@
 #include <iostream>
-#include <string>
 #include <vector>
+#include <stack>
+#include <queue>
 #include <algorithm>
+#include <string>
+#include <unordered_set>
+#include <unordered_map>
+
+// #include "nestedVectorIntVisualization.h"
+// #include "nestedVectorCharVisualization.h"
+// #include "treeVisualizationinPreorder.h"
+// #include "listNodeVisualization.h"
+
+
 using namespace std;
 
-// Method 1:
-//bool compare(string a, string b)
-//{
-//	if (a.size() == b.size())
-//		return a < b;
-//	return a.size() > b.size();  // Descending order
-//}
-
-//class Solution
-//{
-//public:
-//	string findLongestWord(string s, vector<string>& d)
-//	{
-//		sort(d.begin(), d.end(), compare);
-//		for (string currentStr : d)
-//		{
-//			size_t i = 0;
-//			for (char c : s)
-//			{
-//				if (i < currentStr.size() && c == currentStr[i])
-//					i++;
-//			}
-//			if (i == currentStr.size())
-//				return currentStr;
-//		}
-//		return "";
-//	}
-//};
-
-// Method 2:
-class Solution
-{
+// Brute force: time complexity: O(2^N), space complexity: O(2^N)
+class Solution1 {
 public:
-	string findLongestWord(string s, vector<string> &d)
-	{
-		string returnValue = "";
-		for (string currentStr : d)
-		{
-			size_t i = 0;
-			for (char c : s)
-			{
-				if (i < currentStr.size() && c == currentStr[i])
-					i++;
-			}
-			if (i == currentStr.size())
-			{
-				if (currentStr.size() > returnValue.size())
-					returnValue = currentStr;
-				else if (currentStr.size() == returnValue.size() && currentStr < returnValue)
-					returnValue = currentStr;
-			}
-		}
-		return returnValue;
-	}
+    string findLongestWord(string s, vector<string>& d) {
+        unordered_set<string> wordSet{ d.begin(), d.end() };
+        vector<string> wordList;
+        generateWords(s, "", wordList, 0);
+        string res = "";
+        for (string each : wordList)
+        {
+            if (wordSet.count(each) != 0)
+            {
+                if (each.size() > res.size() || (each.size() == res.size() && each[0] < res[0]))
+                    res = each;
+            }
+        }
+        return res;
+    }
+    // DFS
+    void generateWords(string s, string newS, vector<string>& wordList, int i)
+    {
+        if (i == s.size())
+            wordList.push_back(newS);
+        else
+        {
+            generateWords(s, newS + s[i], wordList, i + 1);
+            generateWords(s, newS, wordList, i + 1);
+        }
+    }
+};
+
+
+// Optimized approach: sort and matching
+// Over-write the comparator for sort() function
+bool compare(string a, string b)
+{
+    // Same length return with alphabetical order
+    if (a.size() == b.size())
+        return a < b;
+    else
+        // different length return longer string
+        return a.size() > b.size();
+}
+class Solution2 {
+public:
+    string findLongestWord(string s, vector<string>& d) {
+        sort(d.begin(), d.end(), compare);
+        for (string each : d)
+        {
+            int i = 0;
+            for (char c : s)
+            {
+                if (i < each.size() && each[i] == c)
+                    i++;
+            }
+            if (i == each.size())
+                return each;
+        }
+        return "";
+    }
+};
+
+// Optimized approch: matching without sorting
+class Solution {
+public:
+    string findLongestWord(string s, vector<string>& d) {
+        string res = "";
+        for (string each : d)
+        {
+            int i = 0;
+            // determine whether each is a subsequence of s.
+            for (char ch : s)
+            {
+                if (ch == each[i])
+                    i++;
+            }
+            if (i == each.size())
+            {
+                if (each.size() > res.size() || (each.size() == res.size() && each[0] < res[0]))
+                    res = each;
+            }
+        }
+        return res;
+    }
 };
 
 int main()
 {
-	string s = "abpcplea";
-	vector<string> d(4);
-	d[0] = "a";
-	d[1] = "b";
-	d[2] = "c";
-	d[3] = "p";
-	Solution sol;
-	cout << sol.findLongestWord(s, d) << endl;
-	for (int k = 0; k != d.size(); k++)
-		cout << d[k] << endl;
+    string s = "abpcplea";
+    vector<string> dict = { "ale","apple","monkey","plea" };
+    Solution sol;
+    
+    string res = sol.findLongestWord(s, dict);
+
+    cout << res << endl;
 }
