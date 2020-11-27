7ded72633c84d34588a7972f029848a4de50c2da
yiran <yiransucdr@utexas.edu>
2020-11-27 17:21:21 -0600

    62 DP

---
+++ 62UniquePaths-DP-Medium.py
@@ -0,0 +1,18 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        if m == 0 or n == 0:
+            return 0
+        if m == 1 or n == 1:
+            return 1
+        
+        tmp = [1 for _ in range(n)]
+        dp = [list(tmp) for _ in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        return dp[m - 1][n - 1]
+    
+"""
+https://leetcode.com/problems/unique-paths/
+DP. Unique Path problem. 
+"""
