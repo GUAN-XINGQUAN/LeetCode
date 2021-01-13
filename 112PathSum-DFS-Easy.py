318270d8b773c8ae375457f74bbe489562f0c453
yiran <yiransucdr@utexas.edu>
2021-01-12 22:44:28 -0600

    112 DFS

---
+++ 112PathSum-DFS-Easy.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        if root.left == None and root.right == None and root.val == sum:
+            return True
+        return self.hasPathSum(root.left, sum - root.val) or self.hasPathSum(root.right, sum - root.val)
+"""
+https://leetcode.com/problems/path-sum/
+DFS. What the fuck. [] and 0 needs to be False. 
+"""
